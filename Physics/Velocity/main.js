93aaddb4cc177fb197469ac75cbd940789a96a78
Elvin Ciqueira <elvinciqueira@gmail.com>
2019-10-25 19:57:40 -0300

    velocity

---
+++ Physics/Velocity/main.js
@@ -0,0 +1,42 @@
+window.onload = function() {
+  let canvas = document.getElementById("canvas"),
+    context = canvas.getContext("2d"),
+    width = (canvas.width = window.innerWidth),
+    height = (canvas.height = window.innerHeight),
+    particles = [],
+    numParticles = 100;
+
+  for (let i = 0; i < numParticles; i++) {
+    particles.push(
+      particle.create(
+        width / 2,
+        height / 2,
+        Math.random() * 4 + 1,
+        Math.random() * Math.PI * 2
+      )
+    );
+  }
+
+  update();
+
+  function update() {
+    context.clearRect(0, 0, width, height);
+
+    for (let i = 0; i < numParticles; i++) {
+      let p = particles[i];
+      p.update();
+      context.beginPath();
+      context.arc(
+        p.position.getX(),
+        p.position.getY(),
+        10,
+        0,
+        Math.PI * 2,
+        false
+      );
+      context.fill();
+    }
+
+    requestAnimationFrame(update);
+  }
+};
