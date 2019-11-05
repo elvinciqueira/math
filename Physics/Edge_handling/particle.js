206f713f167e107a9b5cbf0cad3f4181357aaaa6
Elvin Ciqueira <elvinciqueira@gmail.com>
2019-11-05 09:35:15 -0300

    mini

---
+++ Physics/Edge_handling/particle.js
@@ -0,0 +1,25 @@
+const particle = {
+  position: null,
+  velocity: null,
+  gravity: null,
+  radius: 0,
+
+  create: function(x, y, speed, direction, grav) {
+    let obj = Object.create(this);
+    obj.position = vector.create(x, y);
+    obj.velocity = vector.create(0, 0);
+    obj.velocity.setLength(speed);
+    obj.velocity.setAngle(direction);
+    obj.gravity = vector.create(0, grav || 0);
+    return obj;
+  },
+
+  accelerate: function(accel) {
+    this.velocity.addTo(accel);
+  },
+
+  update: function() {
+    this.velocity.addTo(this.gravity);
+    this.position.addTo(this.velocity);
+  }
+};
